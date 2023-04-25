dee2a76d1f7cfd4203c51fb340691c32bb23893e
cosmo4 <warner.luke0102@gmail.com>
2023-04-25 09:27:54 -0600

    finishing up with index.html

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+let newDate = new Date()
+document.getElementById("copyright-year").innerText = newDate.getFullYear();
+
+const modifyDate = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = "Last Modified: " + modifyDate;
