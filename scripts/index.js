16df34fdba0a991275a1c99c646f819c80f7834f
cosmo4 <warner.luke0102@gmail.com>
2023-04-28 14:07:03 -0600

    Added 'visits to page' functionality

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,15 @@ document.getElementById("copyright-year").innerText = newDate.getFullYear();
 
 const modifyDate = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = "Last Modified: " + modifyDate;
+
+var visitCount = localStorage.getItem("page_view");
+
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+} else {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+}
+
+document.getElementById("counterContainer").innerHTML = visitCount;
