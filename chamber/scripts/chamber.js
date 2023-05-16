186b660bc6d049873aa7cd1a594e43cc68405769
cosmo4 <warner.luke0102@gmail.com>
2023-05-16 09:22:13 -0600

    chamber css started

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -3,3 +3,11 @@ document.getElementById("copyright-year").innerText = newDate.getFullYear();
 
 const modifyDate = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = "Last Modified: " + modifyDate;
+
+function toggleNav() {
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('open');
+
+    var menu = document.getElementById('menu');
+    menu.classList.toggle('open');
+}
