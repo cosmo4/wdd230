81424959827e2e05bc96de425df59c9fbd590a11
cosmo4 <warner.luke0102@gmail.com>
2023-05-17 13:43:04 -0600

    dark mode added to home page

--- scripts/index.js
+++ scripts/index.js
@@ -23,3 +23,18 @@ function toggleNav() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('open');
 }
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("html");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
