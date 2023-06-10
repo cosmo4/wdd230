acbb82e213058cacc65f66f608c2baf95ea8149c
cosmo4 <warner.luke0102@gmail.com>
2023-06-10 16:23:36 -0600

    large css commenced

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -11,3 +11,25 @@ function toggleNav() {
     var menu = document.getElementById('menu');
     menu.classList.toggle('open');
 }
+
+var lastVisitDate = localStorage.getItem('lastVisitDate');
+if (!lastVisitDate) {
+  document.getElementById('timing').innerText = 'Welcome! Let us know if you have any questions.';
+  localStorage.setItem('lastVisitDate', new Date().toISOString());
+} else {
+  var currentDate = new Date();
+  var previousVisitDate = new Date(lastVisitDate);
+  var timeDifference = currentDate.getTime() - previousVisitDate.getTime();
+  var daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+
+  var message = '';
+  if (daysDifference < 1) {
+    message = 'Back so soon! Awesome!';
+  } else {
+    message = 'You last visited ' + daysDifference + ' ';
+    message += (daysDifference === 1) ? 'day ago.' : 'days ago.';
+  }
+
+  document.getElementById('timing').innerText = message;
+  localStorage.setItem('lastVisitDate', currentDate.toISOString());
+}
