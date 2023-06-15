b65e22c4ae41fe6e9a317ea9f0bd29d8e9f0ed4f
cosmo4 <warner.luke0102@gmail.com>
2023-06-15 09:33:07 -0600

    fetch api assignment begun

---
+++ week05/scripts/prophets.js
@@ -0,0 +1,33 @@
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+getProphetData = async function() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.prophets);
+}
+
+getProphetData();
+
+displayProphets = function(prophets) {
+    prophets.forEach(prophet => {
+        const card = document.createElement('section');
+        const fullName = document.createElement('h2');
+        const portrait = document.createElement('img');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+        
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}.`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+
+    });
+}
