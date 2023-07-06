d47ca1e6a8ae59a97ab5b557d8b0dcdd3b484cbd
cosmo4 <warner.luke0102@gmail.com>
2023-07-06 15:40:50 -0600

    begun adding forecast content

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -24,7 +24,7 @@ if (!lastVisitDate) {
   var timeDifference = currentDate.getTime() - previousVisitDate.getTime();
   var daysDifference = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-  var message = '';
+  let message = ' ';
   if (daysDifference < 1) {
     message = 'Back so soon! Awesome!';
   } else {
@@ -32,20 +32,21 @@ if (!lastVisitDate) {
     message += (daysDifference === 1) ? 'day ago.' : 'days ago.';
   }
 
-  document.getElementById('timing').innerText = message;
+  let time = document.getElementById('timing');
+  if (time){
+    time.innerText = message;
+  }
+  
+  
   localStorage.setItem('lastVisitDate', currentDate.toISOString());
 }
-document.querySelector('input[type="hidden"]').value = new Date().toISOString();
+let dateValue =document.querySelector('input[type="hidden"]');
+if (dateValue) {
+  dateValue.value = new Date().toISOString();
+} 
+
 
 const membersJSON = 'https://cosmo4.github.io/wdd230/chamber/data/members.json';
-
-function toggleDirectory() {
-
-}
-
-function displayResults(data) {
-
-}
 
 async function jsonFetch() {
   
@@ -53,8 +54,7 @@ async function jsonFetch() {
     const jsonFile = await fetch(membersJSON);
     if (jsonFile.ok) {
       const data = await jsonFile.json();
-      displayResults(data);
-      console.log(data);
+      
     } else {
       throw new Error('Error: Unable to fetch JSON file');
     }
@@ -62,3 +62,75 @@ async function jsonFetch() {
     console.log(error);
   }
 }
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=43.69&lon=-116.49&units=imperial&appid=2c352eee7ef0a34c1b7e420592f48122"
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#weather-caption');
+
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults(data);
+    } else {
+      throw new Error('Error: Unable to fetch weather data');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+
+function displayResults(data) {
+currentTemp.innerHTML = `Temperature: ${data.main.temp.toFixed(0)}&deg;F`;
+const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+const weatherEvents = data.weather.map(event => {
+const description = event.description.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+return description;
+}).join('');
+
+weatherIcon.setAttribute('src', iconsrc);
+weatherIcon.setAttribute('alt', 'Weather Events');
+captionDesc.textContent = `Skies: ${weatherEvents}`;
+console.log(weatherEvents);
+}
+
+// apiFetch();
+
+
+const forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=43.69&lon=-116.49&units=imperial&appid=2c352eee7ef0a34c1b7e420592f48122"
+
+async function apiFetchForecast() {
+  try {
+    const response = await fetch(forecastUrl);
+    if (response.ok) {
+      const data = await response.json();
+      displayResults2(data);
+    } else {
+      throw new Error('Error: Unable to fetch weather data');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+function displayResults2(data) {
+  const filteredList = data.list.filter(item => {
+    const dt = new Date(item.dt * 1000);
+    return dt.getHours() === 12;
+  });
+
+  filteredList.forEach(item => {
+    const date = new Date(item.dt * 1000);
+    const dayofWeek = date.toLocaleDateString(undefined, {weekday: 'short'})
+    const temperature = item.main.temp.toFixed(0);
+    const description = item.weather[0].description;
+
+    console.log(`Date: ${dayofWeek}, Temperature: ${temperature}°F, Description: ${description}`);
+  });
+}
+
+// apiFetchForecast();
